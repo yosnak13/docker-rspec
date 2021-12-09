a2e4e528c28fff5aee4cb5e51b7205908130f397
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-12-10 08:08:03 +0900

    テスト用dbから取得するデータが同一か検証するテスト

--- src/spec/models/food_enquete_spec.rb
+++ src/spec/models/food_enquete_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe FoodEnquete, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '正常系の機能' do
+    context '回答する' do
+      it '正しく登録できること 料理:やきそば food_id: 2,
+                               満足度:良い score: 3,
+                               希望するプレゼント:ビール飲み放題 present_id: 1' do
+
+        enquete = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        #バリデーションが通ることを検証
+        expect(enquete).to be_valid
+        #テストデータの保存
+        enquete.save
+
+        answered_enquete = FoodEnquete.find(1);
+        expect(answered_enquete.name).to eq('田中 太郎')
+        expect(answered_enquete.mail).to eq('taro.tanaka@example.com')
+        expect(answered_enquete.age).to eq(25)
+        expect(answered_enquete.food_id).to eq(2)
+        expect(answered_enquete.score).to eq(3)
+        expect(answered_enquete.request).to eq('おいしかったです。')
+        expect(answered_enquete.present_id).to eq(1)
+      end
+    end
+  end
 end
