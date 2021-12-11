7c450cfb2054d1f3b91dd6b31810770386d3b7db
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-12-11 21:35:43 +0900

    requestカラムの任意が有効であることをテスト

--- src/spec/models/food_enquete_spec.rb
+++ src/spec/models/food_enquete_spec.rb
@@ -52,5 +52,13 @@ RSpec.describe FoodEnquete, type: :model do
         expect(new_enquete.save).to be_falsey
       end
     end
+
+    context '任意入力であること' do
+      it 'ご意見・ご要望が任意であること' do
+        new_enquete = FoodEnquete.new
+        expect(new_enquete).not_to be_valid
+        expect(new_enquete.errors[:request]).not_to include(I18n.t('errors.messages.blank'))
+      end
+    end
   end
 end
