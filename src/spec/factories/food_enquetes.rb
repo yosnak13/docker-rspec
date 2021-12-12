37bf43ccd7ee7d7180f5c44992aefd7ac233de9e
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-12-12 12:15:12 +0900

    :food_enqueteクラスのBot作成

--- src/spec/factories/food_enquetes.rb
+++ src/spec/factories/food_enquetes.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :food_enquete do
-    
+    name { '田中 太郎' }
+    mail { 'taro.tanaka@example.com' }
+    age { 25 }
+    food_id { 2 }
+    score { 3 }
+    request { 'おいしかったです。' }
+    present_id { 1 }
   end
 end
