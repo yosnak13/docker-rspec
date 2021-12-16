550c7a33cf2865cd75484b8993914edeea3bfba0
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-12-17 07:15:27 +0900

    文法エラーを修正

--- src/spec/support/concerns/common_module.rb
+++ src/spec/support/concerns/common_module.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # 共通化するテストケースを定義
 shared_examples '価格の表示' do
   # 動的にモデルを作成
-  let(:object_name) { 'described_class.to_s.underscore.to_sym' }
+  let(:object_name) { described_class.to_s.underscore.to_sym }
   let(:model) { FactoryBot.build(object_name) }
 
   describe '税込み価格が計算されること' do
@@ -18,12 +18,12 @@ shared_examples '価格の表示' do
 end
 
 shared_examples '満足度の表示' do
-  let(:object_name) { 'described_class.to_s.underscore.to_sym'}
+  let(:object_name) { described_class.to_s.underscore.to_sym }
   let(:model) { FactoryBot.build(object_name) }
 
   it '満足度が「悪い」になること' do
     model.score = 1
-    expect(model.view_socre).to eq |I18n.t('common.score.bad')
+    expect(model.view_score).to eq I18n.t('common.score.bad')
   end
 
   it '満足度が「普通」になること' do
