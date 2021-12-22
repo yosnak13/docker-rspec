1aa512c3b9ebf9ca0dfcdfadec8826e0072f9445
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-12-23 07:59:59 +0900

    ジムアンケートアクセスできないことを正とするテスト

--- src/spec/requests/gym_enquetes_spec.rb
+++ src/spec/requests/gym_enquetes_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "GymEnquetes", type: :request do
-  describe "GET /gym_enquetes" do
-    it "works! (now write some real specs)" do
-      get gym_enquetes_path
-      expect(response).to have_http_status(200)
+  describe '正常' do
+    context "アンケートに回答する" do
+      it "ページが表示されないこと" do
+        get "/gym_enquetes/new"
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
